ee70069c77ec2cab86d8b528b50ac492f7ed4cdd
Nguyen Viet Manh <manh.nguyen@thirdsight.net>
2024-11-29 18:05:53 +0800

    Second

---
+++ MockQueryable/TestClass1.cs
@@ -0,0 +1,55 @@
+﻿
+
+namespace MockQueryable;
+
+[TestClass]
+public sealed class TestClass1
+{
+    [AssemblyInitialize]
+    public static void AssemblyInit(TestContext context)
+    {
+        // This method is called once for the test assembly, before any tests are run.
+        Debug.WriteLine(nameof(AssemblyInit));
+    }
+
+    [AssemblyCleanup]
+    public static void AssemblyCleanup()
+    {
+        // This method is called once for the test assembly, after all tests are run.
+        Debug.WriteLine(nameof(AssemblyCleanup));
+    }
+
+    [ClassInitialize]
+    public static void ClassInit(TestContext context)
+    {
+        // This method is called once for the test class, before any tests of the class are run.
+        Debug.WriteLine(nameof(ClassInit));
+    }
+
+    [ClassCleanup]
+    public static void ClassCleanup()
+    {
+        // This method is called once for the test class, after all tests of the class are run.
+        Debug.WriteLine(nameof(ClassCleanup));
+    }
+
+    [TestInitialize]
+    public void TestInit()
+    {
+        // This method is called before each test method.
+        Debug.WriteLine(nameof(TestInit));
+    }
+
+    [TestCleanup]
+    public void TestCleanup()
+    {
+        // This method is called after each test method.
+        Debug.WriteLine(nameof(TestCleanup));
+    }
+
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Debug.WriteLine(nameof(TestMethod1));
+    }
+}
---
+++ MockQueryable2/TestClass2.cs
@@ -0,0 +1,53 @@
+﻿namespace MockQueryable2;
+
+[TestClass]
+public sealed class TestClass2
+{
+    [AssemblyInitialize]
+    public static void AssemblyInit(TestContext context)
+    {
+        // This method is called once for the test assembly, before any tests are run.
+        Debug.WriteLine(nameof(AssemblyInit));
+    }
+
+    [AssemblyCleanup]
+    public static void AssemblyCleanup()
+    {
+        // This method is called once for the test assembly, after all tests are run.
+        Debug.WriteLine(nameof(AssemblyCleanup));
+    }
+
+    [ClassInitialize]
+    public static void ClassInit(TestContext context)
+    {
+        // This method is called once for the test class, before any tests of the class are run.
+        Debug.WriteLine(nameof(ClassInit));
+    }
+
+    [ClassCleanup]
+    public static void ClassCleanup()
+    {
+        // This method is called once for the test class, after all tests of the class are run.
+        Debug.WriteLine(nameof(ClassCleanup));
+    }
+
+    [TestInitialize]
+    public void TestInit()
+    {
+        // This method is called before each test method.
+        Debug.WriteLine(nameof(TestInit));
+    }
+
+    [TestCleanup]
+    public void TestCleanup()
+    {
+        // This method is called after each test method.
+        Debug.WriteLine(nameof(TestCleanup));
+    }
+
+    [TestMethod]
+    public void TestMethod2()
+    {
+        Debug.WriteLine(nameof(TestMethod2));
+    }
+}
